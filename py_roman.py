10c07fb97ed7d18a5ea97ea6d656103d0ef9ff79
Kai Poitschke <kai@poitschke.de>
2018-12-17 16:24:07 +0100

    first commit

---
+++ py_roman.py
@@ -0,0 +1,41 @@
+
+import sys
+import collections
+
+
+def int_to_roman(n):
+    '''Convert an Integer to a Roman Number'''
+    if n < 1:
+        raise ValueError('Not a valid number: ' + str(n))
+    
+    # +------+-----+-----+-----+-----+-----+-----+-----+-----+------+
+    # | Char |  I  |  V  |  X  |  L  |  C  |  D  |  M  |  ↁ  |  ↂ  |
+    # | Value|  1  |  5  | 10  | 50  | 100 | 500 | 1000| 5000| 10000|
+    # +------+-----+-----+-----+-----+-----+-----+-----+-----+------+
+    romans_map = collections.OrderedDict(
+        IIIII= 'V',
+        IIII=  'IV', # subtraction rule
+        VV=    'X',
+        VIV=   'IX',
+        XXXXX= 'L',
+        XXXX=  'XL',
+        LL=    'C',
+        LXL=   'XC',
+        CCCCC= 'D',
+        CCCC=  'CD',
+        DD=    'M',
+        DCD=   'CM',
+        MMMMM= 'ↁ',
+        MMMM=  'Mↁ',
+        ↁↁ=   'ↂ',
+    )
+    
+    roman_str = 'I' * n
+    for roman in romans_map.keys():
+        roman_str = roman_str.replace(roman, romans_map[roman])
+    return roman_str
+
+
+if __name__ == '__main__':
+    for arg in sys.argv[1:]:
+        print(f'{arg} converted to roman = {int_to_roman(int(arg))}')
